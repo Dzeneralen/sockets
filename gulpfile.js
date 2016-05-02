f2d0fe44af2270a273b78233268cfd63ff289b0a
Dzenan <dumpor@gmail.com>
2016-05-03 01:53:03 +0200

    add browsersync for liveload while developing client

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var ts = require("gulp-typescript");
 var autoprefixer = require("gulp-autoprefixer");
 var sourcemaps = require("gulp-sourcemaps");
 var rimraf = require("rimraf");
+var browserSync = require("browser-sync");
 
 var tsProject = ts.createProject("./src/app/tsconfig.json");
 
@@ -15,7 +16,8 @@ gulp.task("compile:sass", function() {
         .pipe(autoprefixer({
             browsers: ["last 5 versions"]
         }))
-        .pipe(gulp.dest("./dist/styles")); 
+        .pipe(gulp.dest("./dist/styles"))
+        .pipe(browserSync.stream()); 
 });
 
 gulp.task("compile:ts", function() {
@@ -28,13 +30,32 @@ gulp.task("compile:ts", function() {
         .pipe(gulp.dest("./dist/app"));
 });
 
+gulp.task("copy:html", function() {
+   return gulp.src("./src/**/*.html")
+        .pipe(gulp.dest("./dist"))
+        .on("change", browserSync.reload);
+        
+});
+
 gulp.task("clean:all", function(callback) {
     rimraf("./dist", callback);
 });
 
-gulp.task("compile:all", ["compile:ts", "compile:sass"]);
+gulp.task("compile:all", ["compile:ts", "compile:sass", "copy:html"]);
 
 gulp.task("watch", ["compile:all"], function() {
    gulp.watch("./src/app/**/*.ts", ["compile:ts"]); 
    gulp.watch("./src/styles/**/*.scss", ["compile:sass"]); 
+   gulp.watch("./src/**/*.html", ["copy:html"]);
+});
+
+gulp.task("serve", ["watch"], function() {
+   browserSync.create();
+   
+   browserSync.init({
+      server: "./dist" 
+   });
+   
+   gulp.watch("./dist/*.html").on("change", browserSync.reload);
+    
 });
